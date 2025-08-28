104804c5f4a4335a169a89b802a13a53a23f9d9d
glp711 <lopesguilherme2912@gmail.com>
2025-08-28 20:30:14 -0300

    adsense

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
+import { useEffect } from "react";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
+
 const Layout = ({ children }: LayoutProps) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3465789567492635";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
   return (
     <div className="min-h-screen bg-background">
       <Header />
